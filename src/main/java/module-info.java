16635aa3cea1a39a665d2d3442453d3490634cbd
Adrian Roszkowski <adr11@hi.is>
2024-03-18 19:51:00 +0000

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module adrian.roszkowski.hopur5tverkefni {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens adrian.roszkowski.hopur5tverkefni to javafx.fxml;
+    exports adrian.roszkowski.hopur5tverkefni;
+}
