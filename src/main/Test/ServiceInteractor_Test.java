51eb41e8c1af52d77fc8314d15d794e3b94f4eb1
Adrian Roszkowski <adr11@hi.is>
2024-03-21 20:02:53 +0000

    Added beforeEach for tests

--- src/main/Test/ServiceInteractor_Test.java
+++ src/main/Test/ServiceInteractor_Test.java
@@ -1,2 +1,11 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
 public class ServiceInteractor_Test {
+
+    Mock mockObject;
+    @BeforeEach
+    void initializeTests() {
+        mockObject = new Mock();
+    }
 }
