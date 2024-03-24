a220eb8bbf99eb237dd344dcfc649f438316971a
Adrian Roszkowski <adr11@hi.is>
2024-03-24 19:55:14 +0000

    Update javadoc in Mock

--- src/main/Test/mock_database/Mock.java
+++ src/main/Test/mock_database/Mock.java
@@ -5,18 +5,32 @@ import java.util.ArrayList;
 public class Mock {
     ArrayList<Service> service;
 
+    /**
+     * Initialize Mock Database
+     */
     public Mock() {
         service = new ArrayList<Service>();
     }
 
+
+    /**
+     * For adding services
+     * @param service the service to add
+     */
     public void addService(Service service) {
         this.service.add(service);
     }
+
 
     public ArrayList<Service> getService() {
         return service;
     }
 
+    /**
+     * For getting a specific service
+     * @param i the index of the service
+     * @return The service
+     */
     public Service getSpecificService(int i) {
         return service.get(i);
     }
